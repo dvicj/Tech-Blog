e493d38b6afb9ec282ff019ae2c60840a6b9668f
dvicjon <devinvjones@gmail.com>
2021-03-12 19:07:14 -0500

    edited and seperated public js functions

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,26 +1,29 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const id = window.location.toString().split('/')[
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document.querySelector('input[name="post-content"]').value;
+  const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-    const response = await fetch(`/api/posts/${id}`, {
+
+  const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+          title,
+          post_content
       }),
       headers: {
-        'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
       }
     });
-  
+    
     if (response.ok) {
       document.location.replace('/dashboard/');
     } else {
       alert(response.statusText);
     }
-  }
-  
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
   
